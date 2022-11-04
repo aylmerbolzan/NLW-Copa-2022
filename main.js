e6f4bae92d00ac747ab050c79a877c9c964c2488
aylmerbolzan <aylmer.bolzan@gmail.com>
2022-11-03 23:24:53 -0300

    Aula 04 - Personalizando o projeto

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.5;
 function createCard(date, day, games) {
+  delay = delay + 0.5;
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
           <h2>${date} <span>${day}</span></h2>
 
           <ul>
@@ -20,26 +22,22 @@ function createCard(date, day, games) {
         `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-        <img src="./assets/logo.svg" alt="Logo NLW Copa" />
-      </header>
+document.querySelector("#cards").innerHTML =
 
-      <main id="cards">
-        ${createCard(
+        createCard(
           "24/11",
           "quinta",
           createGame("brasil", "16:00", "sérvia")
-        )}
-        ${createCard(
+        ) +
+
+        createCard(
           "28/11",
           "segunda",
           createGame("brasil", "13:00", "suíça")
-        )}
-        ${createCard(
+        ) +
+
+        createCard(
           "02/12",
           "sexta",
           createGame("camarões", "16:00", "brasil")
-        )}
-      </main>
-`
+        )
